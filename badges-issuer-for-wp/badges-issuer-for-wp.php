2f62bfe6663a25d055cbee2a152c390deae04cfd
Colomet <colomet@hotmail.com>
2017-09-06 20:06:08 +0200

    link upadate

--- badges-issuer-for-wp/badges-issuer-for-wp.php
+++ badges-issuer-for-wp/badges-issuer-for-wp.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Badges-Issuer-for-wp
  * Plugin URI:        www.badges4languages.com
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           0.6.2
+ * Version:           0.6.3 dev
  * Author:            My language skills team
  * Author URI:        www.badges4languages.com
  * License:           GPL-2.0+
