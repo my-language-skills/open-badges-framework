487d36dbcc2627a2d3c5471513f5541c7cb1da1f
AleRiccardi <alericcardi1996@gmail.com>
2017-10-02 10:34:36 +0200

    Update version

--- badges-issuer-for-wp/uninstall.php
+++ badges-issuer-for-wp/uninstall.php
@@ -20,7 +20,7 @@
  * https://github.com/tommcfarlin/WordPress-Plugin-Boilerplate/pull/123#issuecomment-28541913
  *
  * @link       www.badges4languages.com
- * @since      1.0.0
+ * @since      0.0.1
  *
  * @package    Badges_Issuer_For_Wp
  */
