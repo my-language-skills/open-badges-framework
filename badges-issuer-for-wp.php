49015b2f9e471cb3bec8911bb4a16540030b2df3
AleRiccardi <alericcardi1996@gmail.com>
2017-10-19 21:24:03 +0200

    Right version setted for the plugin

--- badges-issuer-for-wp.php
+++ badges-issuer-for-wp.php
@@ -8,7 +8,7 @@
  * that starts the plugin.
  *
  * @link              www.badges4languages.com
- * @since             0.6.3
+ * @since             X.X.X
  * @package           Badges_Issuer_For_Wp
  *
  * @wordpress-plugin
@@ -16,7 +16,7 @@
  * Plugin URI:        www.badges4languages.com
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin
  * area.
- * Version:           0.6.3 dev
+ * Version:           X.X.X
  * Author:            My language skills team
  * Author URI:        www.badges4languages.com
  * License:           GPL-2.0+
