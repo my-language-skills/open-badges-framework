9262e70370789bbfdd651e253c85d9c93371c513
AleRiccardi <alericcardi1996@gmail.com>
2017-11-09 17:19:09 +0100

    Developing Dashboard page

--- templates/DashboardTemp.php
+++ templates/DashboardTemp.php
@@ -21,7 +21,52 @@ final class DashboardTemp {
         ?>
         <div class="wrap">
             <h1 class="obf-title">Badge Issuer</h1>
-        </div>
+            <div class="container admin">
+                <div class="intro">
+                    <div class="cont-title-dash">
+                        <h2>Action control</h2>
+                        <p class="lead">
+                            This is a simple hero unit, a simple jumbotron-style component for calling extra attention
+                            to featured content or information.
+                        </p>
+                    </div>
+
+                    <div class="">
+                        <div class="add-new-dash">
+                            <div class="col-dash">
+                                <h4>Badges</h4>
+                            </div>
+                            <div class="col-dash">
+                                <p>Add New Badge</p>
+                            </div>
+                        </div>
+                        <div class="add-new-dash">
+                            <div class="col-dash">
+                                <h4>Fields</h4>
+                            </div>
+                            <div class="col-dash">
+                                <p>Add New Field</p>
+                            </div>
+                        </div>
+                        <div class="add-new-dash">
+                            <div class="col-dash">
+                                <h4>Levels</h4>
+                            </div>
+                            <div class="col-dash">
+                                <p>Add New Level</p>
+                            </div>
+                        </div>
+                        <div class="add-new-dash">
+                            <div class="col-dash">
+                                <h4>Classes</h4>
+                            </div>
+                            <div class="col-dash">
+                                <p>Add New Class</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         <?php
     }
 }
