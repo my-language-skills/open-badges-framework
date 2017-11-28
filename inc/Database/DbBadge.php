729936c4513eb626f16ff499b645677ef8dc22dc
AleRiccardi <alericcardi1996@gmail.com>
2017-11-28 15:02:55 +0100

    Added basic function for the class DbModel and DbBadge

--- inc/Database/DbBadge.php
+++ inc/Database/DbBadge.php
@@ -8,28 +8,18 @@
 
 namespace Inc\Database;
 
-global $OBF_db_version;
-$OBF_db_version = "1.0.0";
-
-class DbBadge{
-    static $tableName = 'badge';
-
-    public static function getTableName() {
-        global $wpdb;
-        $tableName = 'obf_badge';
-        return $wpdb->prefix . $tableName;
-    }
+class DbBadge extends DbModel {
+    static $tableName = 'obf_badge';
 
     public function register() {
         global $wpdb;
-        global $OBF_db_version;
 
         $charset_collate = $wpdb->get_charset_collate();
-        $installed_version = get_option('OBF_db_version');
+        $installed_version = get_option(self::DB_NAME_VERSION);
 
-        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 
-        if ($installed_version === $OBF_db_version) {
+
+        if ($installed_version !== self::DB_VERSION) {
             $sql = "CREATE TABLE " . $this->getTableName() . " (
             userEmail varchar(180) NOT NULL,
             badgeId mediumint(9) NOT NULL,
@@ -47,12 +37,122 @@ class DbBadge{
             PRIMARY KEY  (userEmail, badgeId, fieldId, levelId)
         ) $charset_collate;";
 
+            require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
             dbDelta($sql);
 
-            update_option('OBF_db_version', $OBF_db_version);
+            update_option(self::DB_NAME_VERSION, self::DB_VERSION);
         }
     }
 
+    public static function get(array $data = null) {
+        return parent::get($data);
+    }
 
+    public static function getAll() {
+        return parent::get();
+    }
+
+    /**
+     * Insert a badge
+     *
+     * @author      Alessandro RICCARDI
+     * @since       x.x.x
+     *
+     * @param array $data           {
+     *     Optional. Array or query string of arguments for insert a badge.
+     *
+     *     @type string        userEmail        Text.
+     *     @type string        badgeId          Text.
+     *     @type string        fieldId          Text.
+     *     @type string        levelId          Text.
+     *     @type string        classId          Text.
+     *     @type string        teacherId        Text.
+     *     @type string        roleSlug         Text.
+     *     @type string        dateCreation     Text.
+     *     @type string        json             Text.
+     *     @type string        info             Text.
+     *
+     * @return true|false, if errors.
+     */
+    public static function insert(array $data) {
+        $rightKeys = array(
+            'userEmail',
+            'badgeId',
+            'fieldId',
+            'levelId',
+            'classId',
+            'teacherId',
+            'roleSlug',
+            'dateCreation',
+            'json',
+            'info'
+        );
+
+        if(!self::checkKeys($rightKeys, $data)) {
+            return false;
+        } else {
+            return parent::insert($data) === false ? false : true;
+        }
+    }
+
+    /**
+     * Delete a badge
+     *
+     * @author      Alessandro RICCARDI
+     * @since       x.x.x
+     *
+     * @param array $data           {
+     *     Optional. Array or query string of arguments for delete a badge
+     *
+     *     @type string        userEmail        Text.
+     *     @type string        badgeId          Text.
+     *     @type string        fieldId          Text.
+     *     @type string        levelId          Text.
+     *
+     * @return true|false, if errors.
+     */
+    public static function delete(array $data) {
+        $rightKeys = array(
+            'userEmail',
+            'badgeId',
+            'fieldId',
+            'levelId',
+        );
+        if(!self::checkKeys($rightKeys, $data)) {
+            return false;
+        } else {
+            return parent::delete($data);
+        }
+    }
+
+    /**
+     * Check that the array $data contain all the keys
+     * that are inside the array $rightKeys.
+     *
+     * @author      Alessandro RICCARDI
+     * @since       x.x.x
+     *
+     * @param array $rightKeys
+     * @param array $data
+     *
+     * @return true|false, if errors.
+     */
+    private static function checkKeys(array $rightKeys, array $data){
+        $rightDim = count($rightKeys);
+        $count = 0;
+
+        foreach($data as $key => $value) {
+            if(!array_key_exists($key, $rightKeys)){
+                return null;
+            }
+            $count ++;
+        }
+
+        if ($rightDim !== $count) {
+            return null;
+        }
+
+        return true;
+    }
 
 }
--- inc/Database/DbModel.php
+++ inc/Database/DbModel.php
@@ -10,46 +10,66 @@
 
 namespace Inc\Database;
 
-abstract class DbModel {
-    static $primaryKey = 'id';
+class DbModel {
+    const DB_VERSION = '1.0.0';
+    const DB_NAME_VERSION = 'OBF_db_version';
+    static $tableName = "obf_model";
 
     protected static function getTableName() {
         global $wpdb;
-        $tableName = strtolower( get_called_class() );
-        $tableName = str_replace( 'obf_DbModel', 'DbModel', $tableName );
+        $tableName = static::$tableName;
         return $wpdb->prefix . $tableName;
     }
 
-    private static function fetch_sql($value) {
-        global $wpdb;
-        $sql = sprintf('SELECT * FROM %s WHERE %s = %%s', self::getTableName(), static::$primaryKey);
-        return $wpdb->prepare($sql, $value);
+    public static function fetchSql($type = "UPDATE", array $data) {
+        if ($type == "UPDATE") {
+            $sql = "SELECT * FROM " . self::getTableName();
+        } else if ($type == "DELETE") {
+            $sql = "DELETE FROM " . self::getTableName();
+        }
+
+        if ($data == null) {
+            return $sql;
+        } else {
+
+            $length = count($data) - 1;
+            $sql .= " WHERE ";
+
+            foreach ($data as $key => $value) {
+                if (is_string($value)) {
+                    $sql .= "$key = '$value'";
+                } else {
+                    $sql .= "$key = $value";
+                }
+
+                //Add 'AND' only if is not the last field
+                if (array_search($key, array_keys($data)) !== $length) {
+                    $sql .= " AND ";
+                }
+            }
+            return $sql;
+        }
     }
 
-    public static function get($value) {
+    public static function get(array $data = null) {
         global $wpdb;
-        return $wpdb->get_row(self::fetch_sql($value));
+        return $wpdb->get_results(self::fetchSql($data));
     }
 
-    public static function insert($data) {
+    public static function insert(array $data) {
         global $wpdb;
-        $wpdb->insert(self::getTableName(), $data);
+        return $wpdb->insert(self::getTableName(), $data);
     }
 
-    public static function update($data, $where) {
+    public static function update(array $data, array $where) {
         global $wpdb;
         $wpdb->update(self::getTableName(), $data, $where);
     }
 
-    public static function delete($value) {
+    public static function delete(array $data) {
         global $wpdb;
-        $sql = sprintf('DELETE FROM %s WHERE %s = %%s', self::getTableName(), static::$primaryKey);
-        return $wpdb->query($wpdb->prepare($sql, $value));
-    }
-
-    public static function insertId() {
-        global $wpdb;
-        return $wpdb->insert_id;
+        echo $ret = self::fetchSql('DELETE',$data);
+        return $wpdb->query($ret);
     }
 
     public static function timeToDate($time) {
