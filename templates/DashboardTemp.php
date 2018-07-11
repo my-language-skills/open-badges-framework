c8cf2334f56c4e0a6e56f43f9d31eb032551b33b
leocharlier <leo.charlier.music@gmail.com>
2018-07-11 13:00:00 +0200

    Action control style

--- templates/DashboardTemp.php
+++ templates/DashboardTemp.php
@@ -36,8 +36,8 @@ final class DashboardTemp extends BaseController {
     public static function main() {
         ?>
         <div class="wrap">
-            <h1 class="obf-title"><?php _e('Open Badges Framework','open-badges-framework'); ?>
-            </h1>
+            <h1><?php _e('Open Badges Framework','open-badges-framework'); ?></h1>
+
             <ul class="nav nav-tabs">
                 <li class="active"><a href="#tab-1"><?php _e('Action','open-badges-framework'); ?></a></li>
                 <li class=""><a href="#tab-2"> <?php _e('Badges','open-badges-framework'); ?></a></li>
@@ -74,8 +74,8 @@ final class DashboardTemp extends BaseController {
         <div class="container admin">
             <div class="intro-dash">
                 <div class="cont-title-dash">
-                    <h2><?php _e('Action control','open-badges-framework'); ?></h2>
-                    <p class="lead">
+                    <h3 style="margin-bottom: 0px;"><?php _e('Action control','open-badges-framework'); ?></h3>
+                    <p style="margin-bottom: 0px; margin-top: 0px;">
                         <?php _e('Here you can see the statistics of the Badges.','open-badges-framework'); ?>
                     </p>
                 </div>
@@ -83,7 +83,7 @@ final class DashboardTemp extends BaseController {
                 <div class="content-dash">
                     <div class="row-dash">
                         <div class="col-dash">
-                            <h4><?php _e('Badges','open-badges-framework'); ?></h4>
+                            <h3><?php _e('Badges','open-badges-framework'); ?></h3>
                         </div>
                         <div class="col-dash">
                             <div class="vert-hr"></div>
@@ -99,7 +99,7 @@ final class DashboardTemp extends BaseController {
                     </div>
                     <div class="row-dash">
                         <div class="col-dash">
-                            <h4><?php _e('Fields of education','open-badges-framework'); ?></h4>
+                            <h3><?php _e('Fields of education','open-badges-framework'); ?></h3>
                         </div>
                         <div class="col-dash">
                             <div class="vert-hr"></div>
@@ -115,7 +115,7 @@ final class DashboardTemp extends BaseController {
                     </div>
                     <div class="row-dash">
                         <div class="col-dash">
-                            <h4><?php _e('Levels','open-badges-framework'); ?></h4>
+                            <h3><?php _e('Levels','open-badges-framework'); ?></h3>
                         </div>
                         <div class="col-dash">
                             <div class="vert-hr"></div>
@@ -171,10 +171,8 @@ final class DashboardTemp extends BaseController {
         <div class="container admin">
             <div class="intro-dash">
                 <div class="cont-title-dash">
-                    <h2><?php _e('Badges list','open-badges-framework'); ?></h2>
-                    <p class="lead">
-                        
-						<?php _e('In this section you can see all sent badges.','open-badges-framework'); ?>
+                    <h3 style="margin-bottom: 0px;"><?php _e('Badges list','open-badges-framework'); ?></h3>
+                    <p style="margin-bottom: 0px; margin-top: 0px;"><?php _e('In this section you can see all sent badges.','open-badges-framework'); ?></p>
                 </div>
                 <div id="form-badges-list" class="content-dash badges-list-dash">
                     <?php DisplayFunction::badgesTable(); ?>
@@ -199,10 +197,10 @@ final class DashboardTemp extends BaseController {
         <div class="container admin">
             <div class="intro-dash">
                 <div class="cont-title-dash">
-                    <h2><?php _e('Teachers list','open-badges-framework'); ?></h2>
-                    <p class="lead">
-                        
-						<?php _e('In this section you can see the number of badges sent by teachers.','open-badges-framework'); ?>
+                    <h3 style="margin-bottom: 0px;"><?php _e('Teachers list','open-badges-framework'); ?></h3>
+                    <p style="margin-bottom: 0px; margin-top: 0px;">
+                        <?php _e('In this section you can see the number of badges sent by teachers.','open-badges-framework'); ?>
+                    </p>
                 </div>
                 <div id="form-badges-list" class="content-dash badges-list-dash">
                     <!--?php DisplayFunction::badgesTable(); ?> -->
