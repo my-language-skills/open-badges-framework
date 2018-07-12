59d89ce690af81a9f2ccfdfd04d31558a39dfc03
Colomet <colomet@hotmail.com>
2018-07-12 10:24:05 +0200

    new info

--- templates/StaticPagesTemp.php
+++ templates/StaticPagesTemp.php
@@ -37,7 +37,7 @@ final class StaticPagesTemp {
               Once a badge has been sent, the receiver has the possibility to take his badge through a link
               that is inserted in the email.<br>
               After successive steps, the user can get his own badge and also has the possibility to send
-              it to the','open-badges-framework');?> <a href="https://openbadges.org/">Mozilla Open Badges</a> <?php _e('platform.','open-badges-framework');?>
+              it to the','open-badges-framework');?> <a href="https://backpack.openbadges.org/backpack/welcome">Mozilla Open Badges backpack</a> <?php _e('platform.','open-badges-framework');?>
           </p>
           <H2><?php _e('Shortcodes','open-badges-framework');?></h2>
           <p class="lead">
@@ -46,14 +46,18 @@ final class StaticPagesTemp {
               <?php _e('If you need to show just one of the 3 types, you can use the following shortcodes:','open-badges-framework');?><br>
               <b>[send-badge form="a"] </b><?php _e('to send the badge to yourself.','open-badges-framework');?><br>
               <b>[send-badge form="b"] </b><?php _e('to send the badge to one user at a time.','open-badges-framework');?><br>
-              <b>[send-badge form="c"] </b><?php _e('to send the badge to multiple users at a time.','open-badges-framework');?>
-							<b>[send_badge] or [send-badge form="all"] </b>:<?php _e('to have all types available.','open-badges-framework');?>
-							<b>[send-badge ... sec-form="..."] </b>:<?php _e('add a second form (a/b/c) that will be show with the first.','open-badges-framework');?>
+              <b>[send-badge form="c"] </b><?php _e('to send the badge to multiple users at a time.','open-badges-framework');?><br>
+							<b>[send_badge] or [send-badge form="all"] </b>:<?php _e('to have all types available.','open-badges-framework');?><br>
+							<b>[send-badge ... sec-form="..."] </b>:<?php _e('add a second form (a/b/c) that will be show with the first.','open-badges-framework');?><br>
 
           </p>
           <H2><?php _e('Documentation','open-badges-framework');?></h2>
           <p class="lead">
-              <?php _e('The official documentation can be found at','open-badges-framework');?> <a href="https://github.com/my-language-skills/open-badges-framework/blob/master/README.md/">GitHub.</a><br>
+						<?php _e('The official Open Badges documentation can be found at','open-badges-framework');?> <a href="						https://openbadges.org/
+">OpenBadges.</a> or the Open Badges specification at <a href="						https://github.com/mozilla/openbadges-specification
+">Github.</a><br>
+
+              <?php _e('The official Open Badges Framework documentation can be found at','open-badges-framework');?> <a href="https://github.com/my-language-skills/open-badges-framework/blob/master/README.md/">GitHub.</a><br>
               <ul>
                 <li><a href="https://github.com/my-language-skills/open-badges-framework/blob/master/doc/badges-for-languages.md">Badges4Languages</a></li>
                 <li><a href="https://github.com/my-language-skills/open-badges-framework/blob/master/doc/documentation-general.md"><?php _e('General documentation','open-badges-framework');?></a></li>
